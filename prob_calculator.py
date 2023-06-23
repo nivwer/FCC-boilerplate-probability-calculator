0a8c5a6aaa0c9cadd9dd3d791fbe314be8dd87e7
nivwer <nivwerr@gmail.com>
2023-06-23 15:55:40 -0300

    add solution

--- prob_calculator.py
+++ prob_calculator.py
@@ -2,7 +2,36 @@ import copy
 import random
 # Consider using the modules imported above.
 
+
 class Hat:
+
+  def __init__(self, **kwargs):
+    self.contents = []
+    for key, value in kwargs.items():
+      c = (f"{key}," * value).split(",")
+      c.pop(-1)
+      self.contents.extend(c)
+    print(self.contents)
+
+  def draw(self, number):
+    r_contents = []
+    if number > len(self.contents): return self.contents
+    for i in range(number):
+      r = self.contents.pop(int(random.random() * len(self.contents)))
+      r_contents.append(r)
+    return r_contents
 
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+  count = 0
+  for n in range(num_experiments):
+    hat_copy = copy.deepcopy(hat)
+    colors = hat_copy.draw(num_balls_drawn)
+    for v in expected_balls.keys():
+      n = 0
+      for x in range(len(colors)):
+        if colors[x] == v: n += 1
+      if n < expected_balls[v]:
+        count += 1
+        break
+  return 1 - count / num_experiments
--- test_module.py
+++ test_module.py
@@ -3,7 +3,6 @@ import prob_calculator
 
 prob_calculator.random.seed(95)
 class UnitTests(unittest.TestCase):
-    maxDiff = None
     def test_hat_class_contents(self):
         hat = prob_calculator.Hat(red=3,blue=2)
         actual = hat.contents
@@ -21,9 +20,9 @@ class UnitTests(unittest.TestCase):
 
     def test_prob_experiment(self):
         hat = prob_calculator.Hat(blue=3,red=2,green=6)
-        probability = prob_calculator.experiment(hat=hat, expected_balls={"blue":2,"green":1}, num_balls_drawn=4, num_experiments=1000)
+        probability = prob_calculator.experiment(hat=hat, expected_balls={"green":1}, num_balls_drawn=1, num_experiments=1000)
         actual = probability
-        expected = 0.272
+        expected = 6 / 11
         self.assertAlmostEqual(actual, expected, delta = 0.01, msg = 'Expected experiment method to return a different probability.')
         hat = prob_calculator.Hat(yellow=5,red=1,green=3,blue=9,test=1)
         probability = prob_calculator.experiment(hat=hat, expected_balls={"yellow":2,"blue":3,"test":1}, num_balls_drawn=20, num_experiments=100)
